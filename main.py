d590a7b2c4dfab596cb6df33bee2ce34bb63272b
ngzadl <80850706+DosAbicos@users.noreply.github.com>
2025-03-04 22:54:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
+from aiogram.filters import Command
 import asyncio
 import os
 
@@ -8,11 +8,12 @@ API_TOKEN = os.getenv("API_TOKEN")
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-@dp.message(commands=["start"])
-async def start(message: Message):
+@dp.message(Command("start"))
+async def start(message: types.Message):
     await message.answer("Привет, я Totick 🤖")
 
 async def main():
+    print("Bot started...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
